d494b593fb6cef2065be0ceb5497ce900026137b
thisisrahulpal <rahulgurupal@gmail.com>
2024-01-21 22:55:28 +0530

    cors added

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const health = require("./routes/health")
 
 const mongo = require('./util/db')
 const dotenv = require('dotenv');
+const cors = require('cors')
 dotenv.config();
 
 mongo.connect()
+app.use(cors())
 app.use(bodyParser.json());
+
 
 // Use the userRoutes for all user-related routes
 app.use("/api/post", postRoutes);
