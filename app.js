8fb515649ef813f96c0913654014a77351ed55e8
thisisrahulpal <rahulgurupal@gmail.com>
2024-01-22 15:41:39 +0530

    cors change

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 // app.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const postRoutes = require('./routes/postRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const health = require("./routes/health")
+const bodyParser = require("body-parser");
+const postRoutes = require("./routes/postRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const health = require("./routes/health");
 
-const mongo = require('./util/db')
-const dotenv = require('dotenv');
-const cors = require('cors')
+const mongo = require("./util/db");
+const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 
-mongo.connect()
-app.use(cors())
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
-
 
 // Use the userRoutes for all user-related routes
 app.use("/api/post", postRoutes);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
